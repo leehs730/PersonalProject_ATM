318874c7bb3976bf6197b4aaeaf1e4e61c02678c
leehs730 <leehs730@naver.com>
2023-12-13 22:19:34 +0900

    [Fix] 주석 추가 및 안 쓰는 코드 삭제
    설명이 필요한 코드에 주석 추가 및 사용하지 않고 주석 처리된 코드 삭제

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -39,7 +39,7 @@ public class UIManager : MonoBehaviour
         depositMoneyText.text = "예치 금액: " + userData.depositMoney;
     }
 
-    public void OnClickDeposit()
+    public void OnClickDeposit() // 입금 버튼을 클릭 시 입금할 금액 메뉴가 뜸
     {
         chooseButton.SetActive(false);
         inOutbool = true;
@@ -47,44 +47,46 @@ public class UIManager : MonoBehaviour
 
     }
 
-    public void OnClickWithdrawal()
+    public void OnClickWithdrawal() // 출금 버튼을 클릭 시 출금 할 금액 메뉴가 뜸
     {
         chooseButton.SetActive(false);
         inOutbool = false;
         withdrawal.SetActive(true);
     }
 
-    public void OnClickMoney5()
+    public void OnClickMoney5() // 입출금의 5만원을 할당하는 버튼
     {
         CheckInOut(50000);
     }
 
-    public void OnClickMoney3()
+    public void OnClickMoney3() // 입출금의 3만원을 할당하는 버튼
     {
         CheckInOut(30000);
     }
 
-    public void OnClickMoney1()
+    public void OnClickMoney1() // 입출금의 1만원을 할당하는 버튼
     {
         CheckInOut(10000);
     }
 
-    public void CancelButton()
+    public void CancelButton() // 취소 버튼으로 다시 메인 화면으로 돌아감
     {
         deposit.SetActive(false);
         withdrawal.SetActive(false);
         chooseButton.SetActive(true);
     }
 
-    public void CheckInOut(int money)
+    public void CheckInOut(int money) // 입금인지 출금인지 확인과, 초과 금액을 확인하는 메서드
     {
-        if (userData.currentMoney < money && inOutbool) ActiveMessage();
+        // 입금 금액이 현재 금액을 넘은것과 현재 메뉴의 상태가 입금일 시, 입금불가 메시지 팝업창 출력
+        if (userData.currentMoney < money && inOutbool) ActiveMessage(); 
 
+        // 출금 금액이 예치 금액을 넘은 것과 현재 메뉴의 상태가 출금일 시, 출금불가 메시지 팝업창 출력
         else if (userData.depositMoney < money && !inOutbool) ActiveMessage();
 
         else
         {
-            if (inOutbool)
+            if (inOutbool) // 정상 입금 후 매인 메뉴로 이동
             {
                 userData.Deposit(money);
                 deposit.SetActive(false);
@@ -92,7 +94,7 @@ public class UIManager : MonoBehaviour
                 chooseButton.SetActive(true);
             }
 
-            else
+            else // 정상 출금 후 메인 메뉴로 이동
             {
                 userData.Withdrawal(money);
                 withdrawal.SetActive(false);
@@ -102,7 +104,7 @@ public class UIManager : MonoBehaviour
         }
     }        
 
-    public void CustomDeposit()
+    public void CustomDeposit()  // 직접 입금 금액을 입력 후, 초과인지 확인 후 입금
     {
         int dmoney = int.Parse(customDMoneyInput.text);
         if (userData.currentMoney < dmoney && inOutbool) ActiveMessage();
@@ -117,7 +119,7 @@ public class UIManager : MonoBehaviour
         
     }
 
-    public void CustomWithdrawal()
+    public void CustomWithdrawal() // 직접 출금 금액을 입력 후, 초과인지 확인 후 출금
     {
         int wmoney = int.Parse(customWMoneyInput.text);
         if (userData.depositMoney < wmoney && !inOutbool) ActiveMessage();
@@ -131,31 +133,13 @@ public class UIManager : MonoBehaviour
         }        
     }
 
-    public void ActiveMessage()
+    public void ActiveMessage() // 입출금 잔액이 부족하다는 메시지를 팝업창의 텍스트 안에 삽입 후 출력
     {
         Message.SetActive(true);
         MSG.text = inOutbool ? "잔액이 부족합니다." : "출금할 금액이 부족합니다.";
     }
 
-    //public void CheckDMoeny(int money)
-    //{
-    //    if(userData.currentMoney < money)
-    //    {
-    //        Message.SetActive(true);
-    //        MSG.text = "잔액이 부족합니다.";
-    //    }
-    //}
-
-    //public void CheckWMoney(int money)
-    //{
-    //    if(userData.depositMoney < money)
-    //    {
-    //        Message.SetActive(true);
-    //        MSG.text = "출금할 금액이 부족합니다.";
-    //    }
-    //}
-
-    public void TurnOffMessage()
+    public void TurnOffMessage() // 메시지 팝업 창에서 확인 버튼을 누르면 끄도록 하는 버튼 메서드
     {
         Message.SetActive(false);
     }
